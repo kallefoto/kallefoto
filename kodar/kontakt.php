faf7210aa8e0d542442fa6ed33655604091221ca
EndreKlakk <35015439+EndreKlakk@users.noreply.github.com>
2019-03-04 22:05:54 +0100

    forsøk på form

--- kodar/kontakt.php
+++ kodar/kontakt.php
@@ -1,10 +1,9 @@
-
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Kallefoto</title>
     <link rel="stylesheet" href="kontakt.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
@@ -32,12 +31,54 @@
     </nav>
 
     <h1>Kontakt meg her:</h1>
-    <form class="contact_form" action="kontaktform.php" method="post">
-      <input type="text" name="name" value="" placeholder="Navn">
-      <input type="text" name="mail" value="" placeholder="E-Mail">
-      <input type="text" name="subject" value="" placeholder="Emne">
-      <textarea name="message" placeholder="Melding"></textarea>
-      <button type="submit" name="submit">Send</button>
+    <form name="contactform" method="post" action="send_form_email.php">
+    <table width="450px">
+    <tr>
+     <td valign="top">
+      <label for="first_name">First Name *</label>
+     </td>
+     <td valign="top">
+      <input  type="text" name="first_name" maxlength="50" size="30">
+     </td>
+    </tr>
+    <tr>
+     <td valign="top">
+      <label for="last_name">Last Name *</label>
+     </td>
+     <td valign="top">
+      <input  type="text" name="last_name" maxlength="50" size="30">
+     </td>
+    </tr>
+    <tr>
+     <td valign="top">
+      <label for="email">Email Address *</label>
+     </td>
+     <td valign="top">
+      <input  type="text" name="email" maxlength="80" size="30">
+     </td>
+    </tr>
+    <tr>
+     <td valign="top">
+      <label for="telephone">Telephone Number</label>
+     </td>
+     <td valign="top">
+      <input  type="text" name="telephone" maxlength="30" size="30">
+     </td>
+    </tr>
+    <tr>
+     <td valign="top">
+      <label for="comments">Comments *</label>
+     </td>
+     <td valign="top">
+      <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+     </td>
+    </tr>
+    <tr>
+     <td colspan="2" style="text-align:center">
+      <input type="submit" value="Submit">   <a href="http://www.freecontactform.com/email_form.php">Email Form</a>
+     </td>
+    </tr>
+    </table>
     </form>
 
 </header>
